1a28f7783b694a1035f06c6fa9f7159322ca0235
Jiangdong Chen <mechleechan@outlook.com>
2021-08-13 11:23:59 +0800

    add file size check

--- rer_record.py
+++ rer_record.py
@@ -9,6 +9,10 @@ from datetime import datetime
 from setting import minute_segments
 
 
+def get_size(file_path):
+    return int(os.path.getsize(file_path))
+
+
 def cam_record(root_dir):
     print("Record Service.")
     while True:
@@ -28,7 +32,8 @@ def cam_record(root_dir):
                 fps = int(cap.get(cv2.CAP_PROP_FPS))
                 width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
                 height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-                out_dir = os.path.join(root_dir, time_now.strftime("%Y-%m-%d-%H-%M") + '.avi')
+                out_dir_base = os.path.join(root_dir, time_now.strftime("%Y-%m-%d-%H-%M") + '.avi')
+                out_dir = out_dir_base[:]
 
                 out = cv2.VideoWriter(out_dir, fourcc, fps, (int(width), int(height)))
                 print("Start Recording.")
@@ -41,14 +46,15 @@ def cam_record(root_dir):
                     # print("Debug: Successfully retieve frame.")
                     # cv2.imshow('frame', frame)
                     sleep(0.01)
-                    try:
+                    if get_size(out_dir) <= 1073741824:  # 1GB
                         out.write(frame)
-                    except Exception:  # Acchieve maximum size - 2GB
+                    else:  # Note: maximum recording size - 2GB
                         cnt += 1
                         out.release()
-                        out_dir_suffix = out_dir.split('.avi')[0] + '_' + str(cnt) + '.avi'
-                        out = cv2.VideoWriter(out_dir_suffix, fourcc, fps, (int(width), int(height)))
+                        out_dir = out_dir_base.split('.avi')[0] + '_' + str(cnt) + '.avi'
+                        out = cv2.VideoWriter(out_dir, fourcc, fps, (int(width), int(height)))
                         out.write(frame)
+
 
                 print("Stop Recording.")
                 cap.release()
